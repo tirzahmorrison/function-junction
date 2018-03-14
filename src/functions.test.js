242832766d989c4b2199ee2b6836527b35333bc0
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-03-14 15:51:47 -0400

    z

--- src/functions.test.js
+++ src/functions.test.js
@@ -5,16 +5,16 @@
  */
 
 // ...
-const max = (x, y) => { 
-  if (isNaN(x) && isNaN(y)) { 
-    return NaN 
+const max = (x, y) => {
+  if (isNaN(x) && isNaN(y)) {
+    return NaN
   }
   if (isNaN(x)) { return y }
   if (isNaN(y)) { return x }
-  if (x > y) { 
-    return x 
+  if (x > y) {
+    return x
   }
-  return y 
+  return y
 }
 
 /**
@@ -24,12 +24,12 @@ const max = (x, y) => {
 
 // ...
 const maxOfThree = (x, y, z) => {
-  const maxOfxy = max (x, y)
-  return max (maxOfxy, z)
+  const maxOfxy = max(x, y)
+  return max(maxOfxy, z)
 }
 
-    
-  
+
+
 
 /*
  * Define a function sum() that takes two numbers as
@@ -37,7 +37,7 @@ const maxOfThree = (x, y, z) => {
  */
 
 // ...
-const sum = (x,y) => {
+const sum = (x, y) => {
   return x + y;
 }
 
@@ -62,104 +62,110 @@ const sumOfArray = (arr) => {
  */
 
 // ...
-const isVowel = ()
+const isVowel = (letter) {
+  const vowels = ["a", "e", "i", "o", "u"]
+  for (let i = 0; i < vowels.length; i++) {
+    if (letter === vowels[i]) {
+      return true;
+    }
+    return false;
+  };
+
+  /**
+   * Write a function rovarspraket() that will translate
+   * a text into a "rövarspråket". That is, double every
+   * consonant and place an occurrence of "o" in between.
+   *
+   * For example, rovarspraket("this is fun") should
+   * return the string "tothohisos isos fofunon".
+   */
+
+  // ...
+
+  /**
+   * Define a function reverse() that computes
+   * the reversal of a string. For example,
+   * reverse("skoob") should return the
+   * string "books".
+   */
+
+  // ...
 
 
-/**
- * Write a function rovarspraket() that will translate
- * a text into a "rövarspråket". That is, double every
- * consonant and place an occurrence of "o" in between.
- *
- * For example, rovarspraket("this is fun") should
- * return the string "tothohisos isos fofunon".
- */
-
-// ...
-
-/**
- * Define a function reverse() that computes
- * the reversal of a string. For example,
- * reverse("skoob") should return the
- * string "books".
- */
-
-// ...
 
 
 
-  
+  /**
+   * Write a function findLongestWord() that takes an
+   * string returns the first, longest word in the array.
+   *
+   * i.e. findLongestWord("book dogs") should return "book"
+   */
 
-/**
- * Write a function findLongestWord() that takes an
- * string returns the first, longest word in the array.
- *
- * i.e. findLongestWord("book dogs") should return "book"
- */
+  // ...
 
-// ...
+  /**
+   * NOTE: Don't modify anything below this line...
+   */
 
-/**
- * NOTE: Don't modify anything below this line...
- */
+  /* eslint-disable no-undef */
 
-/* eslint-disable no-undef */
+  import test from 'ava'
 
-import test from 'ava'
+  test('max()', (t) => {
+    t.is(max(1, 3), 3)
+    t.is(max(0, 3), 3)
+    t.is(max(10, 3), 10)
+    t.is(max(-1, -3), -1)
+    t.is(max('aaa', 0), 0)
+    t.true(isNaN(max('aaa', 'bbb')))
+  })
 
-test('max()', (t) => {
-  t.is(max(1, 3), 3)
-  t.is(max(0, 3), 3)
-  t.is(max(10, 3), 10)
-  t.is(max(-1, -3), -1)
-  t.is(max('aaa', 0), 0)
-  t.true(isNaN(max('aaa', 'bbb')))
-})
+  test('maxOfThree()', (t) => {
+    t.is(maxOfThree(1, 3, 2), 3)
+    t.is(maxOfThree(0, 3, -1), 3)
+    t.is(maxOfThree(10, 3, 50), 50)
+    t.is(maxOfThree(-1, -3, -10), -1)
+    t.is(maxOfThree('aaa', 0, 1), 1)
+    t.true(isNaN(maxOfThree('aaa', 'bbb', 'ccc')))
+  })
 
-test('maxOfThree()', (t) => {
-  t.is(maxOfThree(1, 3, 2), 3)
-  t.is(maxOfThree(0, 3, -1), 3)
-  t.is(maxOfThree(10, 3, 50), 50)
-  t.is(maxOfThree(-1, -3, -10), -1)
-  t.is(maxOfThree('aaa', 0, 1), 1)
-  t.true(isNaN(maxOfThree('aaa', 'bbb', 'ccc')))
-})
+  test('sum()', (t) => {
+    t.is(sum(8, 11), 19)
+    t.is(sum(4, 100), 104)
+  })
 
-test('sum()', (t) => {
-  t.is(sum(8, 11), 19)
-  t.is(sum(4, 100), 104)
-})
+  test('sumOfArray()', (t) => {
+    t.is(sumOfArray([1, 2]), 3)
+    t.is(sumOfArray([1, 2, 3]), 6)
+    t.is(sumOfArray([10, 9, 8]), 27)
+    t.is(sumOfArray([]), 0)
+  })
 
-test('sumOfArray()', (t) => {
-  t.is(sumOfArray([1, 2]), 3)
-  t.is(sumOfArray([1, 2, 3]), 6)
-  t.is(sumOfArray([10, 9, 8]), 27)
-  t.is(sumOfArray([]), 0)
-})
+  test('isVowel()', (t) => {
+    t.is(isVowel(0), false)
+    t.is(isVowel('B'), false)
+    t.is(isVowel('b'), false)
+    t.is(isVowel('a'), true)
+    t.is(isVowel('E'), true)
+  })
 
-test('isVowel()', (t) => {
-  t.is(isVowel(0), false)
-  t.is(isVowel('B'), false)
-  t.is(isVowel('b'), false)
-  t.is(isVowel('a'), true)
-  t.is(isVowel('E'), true)
-})
+  test('rovarspraket()', (t) => {
+    t.is(rovarspraket('a'), 'a')
+    t.is(rovarspraket('b'), 'bob')
+    t.is(rovarspraket('cat'), 'cocatot')
+    t.is(rovarspraket('javascript'), 'jojavovasoscocroripoptot')
+    t.is(rovarspraket(0), '0')
+  })
 
-test('rovarspraket()', (t) => {
-  t.is(rovarspraket('a'), 'a')
-  t.is(rovarspraket('b'), 'bob')
-  t.is(rovarspraket('cat'), 'cocatot')
-  t.is(rovarspraket('javascript'), 'jojavovasoscocroripoptot')
-  t.is(rovarspraket(0), '0')
-})
+  test('reverse()', (t) => {
+    t.is(reverse('books'), 'skoob')
+    t.is(reverse("we don't want no trouble"), "elbuort on tnaw t'nod ew")
+  })
 
-test('reverse()', (t) => {
-  t.is(reverse('books'), 'skoob')
-  t.is(reverse("we don't want no trouble"), "elbuort on tnaw t'nod ew")
-})
-
-test('findLongestWord()', (t) => {
-  t.is(findLongestWord('book dogs'), 'book')
-  t.is(findLongestWord('everything'), 'life the universe and everything')
-})
+  test('findLongestWord()', (t) => {
+    t.is(findLongestWord('book dogs'), 'book')
+    t.is(findLongestWord('everything'), 'life the universe and everything')
+  })
 
 /* eslint-enable */
